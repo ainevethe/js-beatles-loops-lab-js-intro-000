3c322cd1096f563129a91c72f669825abd062e38
Crista Cooper <aine3d@gmail.com>
2017-10-13 14:59:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
-function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musician, instrument) {
   var Beatles = {};
+
+  for (var = i; i <= Beatles.length; i++) {
+    Beatles.push(musician[i] + "plays " + instrument[i]);
+    console.log(Beatles[i]);
+  }
 }
